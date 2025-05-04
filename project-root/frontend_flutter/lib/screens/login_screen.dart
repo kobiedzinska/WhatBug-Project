b2150aed92b1f5177e9b71add6b15f96c28c53dd
zuzuz <duda.zuzanna7@gmail.com>
2025-05-04 20:04:29 +0200

    basic login screen without login checking

--- project-root/frontend_flutter/lib/screens/login_screen.dart
+++ project-root/frontend_flutter/lib/screens/login_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:frontend_flutter/screens/home_screen.dart';
+import 'package:frontend_flutter/utilities/my_app_bar.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -8,9 +10,101 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  void attemptLogin() {
+    String email = emailController.text.trim();
+    String password = passwordController.text.trim();
+
+    bool loginSuccessful = true;
+    // TODO implement login check
+
+    if (email.isEmpty || password.isEmpty) {
+      _showErrorDialog("Error", "Please enter both email and password.");
+      return;
+    }
+
+    if (loginSuccessful) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) {
+            return HomePage();
+          },
+        ),
+      );
+    }
+  }
+
+  void _showErrorDialog(String title, String message) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(message),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text("OK"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: MyAppBar(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text('Logowanie', style: TextStyle(fontSize: 25)),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.80,
+                child: TextField(
+                  controller: emailController,
+                  decoration: const InputDecoration(
+                    labelText: 'Email',
+                    border: OutlineInputBorder(),
+                  ),
+                  autofocus: true,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.80,
+                child: TextField(
+                  controller: passwordController,
+                  decoration: const InputDecoration(
+                    labelText: 'Hasło',
+                    border: OutlineInputBorder(),
+                  ),
+                  obscureText: true,
+                ),
+              ),
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.25,
+              child: ElevatedButton(
+                onPressed: attemptLogin,
+                child: const Text('Zaloguj', style: TextStyle(fontSize: 18)),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
