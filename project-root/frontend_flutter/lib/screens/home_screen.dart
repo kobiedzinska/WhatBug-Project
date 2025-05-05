c6ff7954c1feb54c9f1365f58de82faef9f4421a
zuzuz <duda.zuzanna7@gmail.com>
2025-05-05 15:07:43 +0200

    added bug card and home screen implementation

--- project-root/frontend_flutter/lib/screens/home_screen.dart
+++ project-root/frontend_flutter/lib/screens/home_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:frontend_flutter/utilities/bottom_bar.dart';
+import 'package:frontend_flutter/utilities/bug_card.dart';
 import 'package:frontend_flutter/utilities/camera_button.dart';
 import 'package:frontend_flutter/utilities/my_app_bar.dart';
-import 'package:frontend_flutter/utilities/navigation.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,35 +13,93 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // TODO get data of recently found bugs
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const MyAppBar(),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Column(
-                    children: [
-                      const Text(
-                        'welcome to our app',
-                        style: TextStyle(fontSize: 25),
-                      ),
-                      const Text(
-                        '☆*: .｡. o(≧▽≦)o .｡.:*☆',
-                        style: TextStyle(fontSize: 25),
-                      ),
-                    ],
-                  ),
-                  Image(image: AssetImage('lib/images/appIcon.png')),
-                ],
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 20),
+              Divider(
+                thickness: 4,
+                indent: MediaQuery.of(context).size.width * 0.05,
+                endIndent: MediaQuery.of(context).size.width * 0.05,
               ),
-            ),
-          ],
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.90,
+                child: Row(
+                  children: [
+                    Icon(Icons.lightbulb_outline_rounded, size: 30),
+                    Text(
+                      'Did you know... ?',
+                      style: GoogleFonts.comicNeue(
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Card(
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.90,
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            'Odorek zielony, jeśli zostanie wystraszony, wydziela mocny, nieprzyjemny zapach by odstraszyć potencjalne niebezpieczeństwo - stąd jego nazwa.',
+                            style: GoogleFonts.comicNeue(
+                              fontSize: 20,
+                              fontWeight: FontWeight.normal,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Divider(
+                thickness: 4,
+                indent: MediaQuery.of(context).size.width * 0.05,
+                endIndent: MediaQuery.of(context).size.width * 0.05,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.90,
+                child: Text(
+                  'Recently found',
+                  style: GoogleFonts.comicNeue(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+              ),
+              BugCard(
+                bugName: 'La kukaracza',
+                bugInfo: 'nie ma tak że dobrze albo że niedobrze',
+                bugPicture: AssetImage('lib/images/appIcon.png'),
+              ),
+              BugCard(
+                bugName: 'Jelonek rogacz',
+                bugInfo: 'jest całkiem średnio',
+                bugPicture: AssetImage('lib/images/appIcon.png'),
+              ),
+              BugCard(
+                bugName: 'Cymbałek jakiś',
+                bugInfo: 'bążur',
+                bugPicture: AssetImage('lib/images/appIcon.png'),
+              ),
+              SizedBox(height: 110),
+            ],
+          ),
         ),
       ),
       floatingActionButton: CameraButton(),
---
+++ project-root/frontend_flutter/lib/utilities/bug_card.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class BugCard extends StatelessWidget {
+  const BugCard({
+    super.key,
+    required this.bugName,
+    required this.bugInfo,
+    required this.bugPicture,
+  });
+  final String bugName;
+  final String bugInfo;
+  final AssetImage bugPicture;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width * 0.90,
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image(
+                  image: bugPicture,
+                  width: MediaQuery.of(context).size.width * 0.25,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.55,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          bugName,
+                          style: GoogleFonts.comicNeue(
+                            fontSize: 28,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(
+                          child: Text(
+                            bugInfo,
+                            style: GoogleFonts.comicNeue(
+                              fontSize: 20,
+                              fontWeight: FontWeight.normal,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
